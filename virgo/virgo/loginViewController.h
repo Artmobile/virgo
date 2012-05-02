b1dce02344add59cbdad92f58ef16b18953e2832
Artmobile <artmobile@gmail.com>
2012-05-02 11:21:58 +0300

    Started virgo

---
+++ virgo/virgo/loginViewController.h
@@ -0,0 +1,13 @@
+//
+//  loginViewController.h
+//  virgo
+//
+//  Created by Ilya Alberton on 5/2/12.
+//  Copyright 2012 artmobile@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface loginViewController : UIViewController
+
+@end
